b56a49df2a0099c089fb6ef4ea0f26d93630fa57
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-04-17 15:02:46 +0000

    Fahrenheit conversion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 BrandonBCode All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: BrandonBCode
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the celsius from fahrenheit
+ */
+function calculateCelsius() {
+  // input that takes the fahrenheit from the user and converts it.
+  let fahrenheitA = parseFloat(document.getElementById('fahrenheit').value);
+  
+
+  //calculates celsius from fahrenheit
+    let celsiusA = (fahrenheitA - 32) * 5 / 9;
+
+  // output that displays the celsius
+    document.getElementById("fahrenheit-convo").innerHTML = "The Celsius is: " + celsiusA.toFixed(1) + "°";
+
+}
